830163fdc7d1be10ab86113b5d48373c7ef0a0cb
Dert12318 <devanrt140999@gmail.com>
2022-01-02 21:32:33 +0700

    fixing module import in main godertlog

---
+++ godertlog.go
@@ -0,0 +1,10 @@
+package godertlog
+
+import (
+	"github.com/Dert12318/Go-DERT-Log/connection"
+	"github.com/Dert12318/Go-DERT-Log/log"
+)
+
+var logging log.LogMenu
+var connectionElastic connection.ElasticConfig
+var connectionPostgres connection.PostgresConfig
